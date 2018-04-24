a1dd7c0bdc8ebaf85034e6f37f2751ee802d230a
장만호 <jmh3360@gmail.com>
2018-04-24 20:48:55 +0900

    Manho

--- bookk/src/main/webapp/resources/js/shop.js
+++ bookk/src/main/webapp/resources/js/shop.js
@@ -27,6 +27,7 @@ var shop=shop || {};
 }}*/
 shop.mall = {cart:x=>{
 	$.getScript(x.view,()=>{
+		
 		$('#div-advertise').html(createDiv({id:'div-content',clazz:''}));
 		$('#div-content').html(createDiv({id:'div-wrap-root',clazz:'wrap-root'}));
 		$('#div-wrap-root').append(createUL({id:'ul-root',clazz:'list-inline root'}));
@@ -36,14 +37,10 @@ shop.mall = {cart:x=>{
 			$('#div-content').append(createDiv({id:'div-cart-title',clazz:''}))
 			$('#div-content').append(createDiv({id:'div-cart-content',clazz:''}))
 			$('#div-content').attr('style','width:1000px; margin: 0 auto');
-			/*$.ajax({
-			url:x.context+'/cartcount/userid',
-			method:'POST',
-			data:JSON.stringify({userid:'장만호'}),
-			dataType:'json',
-			contentType:'application/json',
-			success:x=>{*/
+
+		
 			var id ={userid:'ju'}
+			
 					$.ajax({
 					url:x.context+'/cartlist/'+id.userid+'',
 					method:'POST',
@@ -52,28 +49,39 @@ shop.mall = {cart:x=>{
 					dataType:'json',
 					contentType:'application/json',
 					success:y=>{
-						console.log(y,'1');
-						console.log(Object.keys(y[0]).length,'1');
+						
+						
+						console.log(y);
 						console.log(Object.keys(y).length,'리스트의 개수');
 
+						
 						$('#div-cart-title').html(createHTag({size:'2',val:'장바구니 '+Object.keys(y).length+'건'}))
 							$('#div-cart-title').attr('style','border-bottom:2px solid black; text-align: center')
 							$(createTable({id:'table-cart',clazz:'table-cart'})).appendTo('#div-cart-content')
 							$(createTr({id:'tr-head'})).appendTo('#table-cart')
 							$(createMultiTh({arr:['1','2','3','4'],clazz:'',id:'th'})).appendTo('#tr-head')
-							$('#th-0').html(createInput({id:'ip-check-all',clazz:'',type:'checkbox'})).attr('style','width:5%')
+							$('#th-0').html(createInput({id:'ip-check-all',clazz:'update',type:'checkbox'})).attr('style','width:5%;padding-right:30px;')
+							$('#ip-check-all').attr('checked','checked')
 							$('#th-1').text('상품정보').attr('style','width:65%')
 							$('#th-2').text('상품금액')
 							$('#th-3').text('삭제').attr('colspan','3').attr('style','width:10%');
+						//tr의 길이
 							$(createMultiTr({id:'tr-cart-list',arr:makeCount(Object.keys(y).length)}))
 							.appendTo('#table-cart');
-		
+							
 						for(var i =0; i<=Object.keys(y).length-1;i++){
+							//멀티 td 영역
 							$('#tr-cart-list-'+i+'').append(createMultiTd({
 								id:'td-tr'+i+'-cart-list',
-								arr:makeCount(Object.keys(y[0]).length-4)}))
+								arr:makeCount(Object.keys(y[0]).length-5)}))
+							$(createInput({id:'book-check-'+i+'',type:'checkbox',clazz:'update'})).appendTo('#td-tr'+i+'-cart-list-0')
+							.attr('name','book-check').attr('checked','checked')
 							$('#td-tr'+i+'-cart-list-1').append(createDiv({
 								id:'div-cart-list-content-'+i+'',clazz:''}))
+								
+							$(createButton({id:'btn-book-del-'+i+'',clazz:'btn',val:'삭제'})).appendTo('#td-tr'+i+'-cart-list-3')
+							.attr('style','background-color:#ce6a52;color:white;width:50%;margin-left:25%');
+							
 							$('#div-cart-list-content-'+i+'').append(createMultiDiv({
 								id:'div-tr'+i+'-td1-cart-list',
 								arr:makeCount(3),
@@ -81,155 +89,187 @@ shop.mall = {cart:x=>{
 							
 							
 							$(createImage({id:'',src:y[i].imageRoute,clazz:''})).appendTo('#div-tr'+i+'-td1-cart-list-0')
-							.attr('style','width:80px;height:120px;margin-top:20px')
+							.attr('style','width:80px;height:120px;margin-top:20px');
+							$('#div-tr'+i+'-td1-cart-list-0').attr('style','margin-right:30px;margin-bottom:10px;');
 							$('#div-tr'+i+'-td1-cart-list-1').append(strong({id:'strong-'+i+'',val:y[i].bookName}))
+							.attr('style','margin-top:7%;margin-right:40px;width:300px;font-size:14px');
 							
 							$('#div-tr'+i+'-td1-cart-list-1').append(createP({id:'p-'+i+'',val:y[i].writter}));
-							$('#div-tr'+i+'-td1-cart-list-2').append(createInput({id:'input-amount',clazz:'text'}))
+							$('#div-tr'+i+'-td1-cart-list-2').append(createInput({id:'input-amount-'+i+'',type:'text',clazz:'update'}))
+							.attr('style','margin-top:10%');
+							$('#input-amount-'+i+'').attr('value',y[i].amount)
+							.attr('style','width:100px;');
+							$('#td-tr'+i+'-cart-list-2').text(y[i].price).attr('style', 'text-align: center');
+								$('#input-amount-'+i+'').spinner({
+								min:0,
+								max:10,
+								
+								});
+								
+								
+							$('.ui-spinner').attr('style','background: white');
 							
+						
+								
+						
 							
-							$('#td-tr'+i+'-cart-list-2').text(y[i].amount);
-							$('#td-tr'+i+'-cart-list-4').text(y[i].price);
-							
+						}//for문 끝나는 지점
+						
+						
+						$('#ip-check-all').change(()=>{
+							if($('#ip-check-all').is(":checked")){
+								$('input[type=checkbox]').prop('checked',true);
+							}else if(!($('#ip-check-all').is(":checked"))){
+								$('input[type=checkbox]').prop('checked',false);
+							}
+						});
+					
+						
+						
+						$('#div-content').append(createDiv({id:'div-cart-footer',clazz:''}))
+						$(createDiv({id:'div-cart-footer-content',clazz:''})).appendTo('#div-cart-footer')
+						.attr('style','padding-top: 20px');
+						$('#div-cart-footer-content').append(createDiv({
+							id:'div-cart-footer-content-left',
+							clazz:''}))
+						$('#div-cart-footer-content').append(createDiv({
+							id:'div-cart-footer-content-right',
+							clazz:''}))
+							$('#div-cart-footer-content-left').text('부크크의 도서의 경우 주문제작 상품이기 때문에 결제 후 4~5일 정도 배송에 소요됩니다.')
+							$('#div-cart-footer-content-right').append(createButton({
+								id:'btn-select-del',
+								clazz:'btn',val:'선택한 도서 삭제'}))
+								$('#btn-select-del').attr('style','background-color:#f8f8f8;color:black;border-color:#c8c8c8');
+						//주문 금액 영역
+						$('#div-content').append(createDiv({id:'div-cart-price',clazz:''}))
+						$('#div-cart-price').append(createTable({id:'table-cart-price',clazz:'table-cart-price'}))
+						$('#table-cart-price').append(createMultiTr({arr:['',''],id:'tr-cart-price'}))
+						$('#tr-cart-price-0').append(createMultiTh({id:'th-price-head',arr:['1','2','3']}))
+						$('#th-price-head-0').text('주문금액');
+						$('#th-price-head-1').text('배송비');
+						$('#th-price-head-2').text('결제 예정금액');
+						$('#tr-cart-price-1').append(createMultiTd({arr:['','',''],id:'td-cart-price'}));
+						$('#td-cart-price-0').append(createSpan({id:'span-td-0',clazz:''})+'원');
+						var val = 0
+						for(var i =0;i<Object.keys(y).length;i++){
+						val +=	y[i].amount*y[i].price*1
 						}
-						/*$('#td-tr'+i+'-cart-list-1').apend(createImage({id:'',src:y[i].imageRoute,clazz:''}))*/
+						$('#span-td-0').text(val).attr('style','font-size:30px');
 						
 						
-					
 						
 						
-					
+						$('#td-cart-price-1').append(createSpan({id:'span-td-1',clazz:''})+'원');
+						$('#span-td-1').attr('style','font-size:30px');
+						$('#td-cart-price-2').append(createSpan({id:'span-td-2',clazz:''})+'원');
+						$('#span-td-2').attr('style','font-size:30px');
+						
+						//배송 checkbox div
+						$('#div-content').append(createDiv({id:'div-delivery-price'}))
+						$('#div-delivery-price').append(createMultiDiv({id:'div-delivery-price',arr:['','']}));
+						$('#div-delivery-price').attr('font-weight','normal');
+						
+						
+						$('#div-delivery-price-0').append(createLabel({
+							fo:'',
+							val:createInput({
+								id:'input-checkbox-0',
+								clazz:'',
+								type:'checkbox'})+'배송료를 선불로 지불합니다. (기본 2,500원)'}));
+						$('#input-checkbox-0').attr('value','2500');
+						$('#input-checkbox-0').attr('checked','checked');
+						$('#div-delivery-price-1').append(createLabel({
+							fo:'',
+							val:createInput({
+								id:'input-checkbox-1',
+								clazz:'',
+								type:'checkbox'})+'제주도 및 도서산간지역(+2,500원)'}));
+						$('#input-checkbox-1').attr('value','2500');
+						$('#span-td-1').text('2500');
+						$('#span-td-2').text($('#span-td-0').text()*1+($('#span-td-1').text())*1);
+						$('input[id=input-checkbox-0]').click(()=>{
+							if ($('input[id=input-checkbox-0]').is(':checked')) {
+								
+								$('input[id=input-checkbox-1]').removeAttr('disabled');
+								$('#span-td-1').text('2500');
+								$('input[id=input-checkbox-0]').attr('value','2500');
+								$('#span-td-2').text($('#span-td-0').text()*1+($('#span-td-1').text())*1);
+								
+								
+							} else {
+								
+								$('#span-td-1').text('0');
+								$('input[id=input-checkbox-1]').prop('checked',false);
+								$('input[id=input-checkbox-1]').attr('disabled','disabled');
+								$('#span-td-2').text($('#span-td-0').text()*1+($('#span-td-1').text())*1);
+							}
+						});
+						$('input[id=input-checkbox-1]').click(()=>{
+							if ($('input[id=input-checkbox-1]').is(':checked')) {
+								
+								$('input[id=input-checkbox-1]').removeAttr('disabled');
+								$('input[id=input-checkbox-1]').attr('value','2500');
+								$('#span-td-1').text(($('input[id=input-checkbox-0]').val())*1+($('input[id=input-checkbox-1]').val())*1);
+								$('#span-td-2').text($('#span-td-0').text()*1+($('#span-td-1').text())*1);
+								 
+							}else{
+								
+								$('#span-td-1').text(parseInt($('input[id=input-checkbox-0]').val()));
+								$('#span-td-2').text($('#span-td-0').text()*1+($('#span-td-1').text())*1);
+							}
+						});
+						$('.ui-spinner-button').addClass('update');
+						var zz = 0;
+						$('.update').click(()=>{
+							$('#span-td-0').text(zz).attr('style','font-size:30px');
+							zz =0;
+							$(function(){
+								if($('#ip-check-all').is(':checked')){
+									calc();
+								} else {
+									
+									$('#span-td-0').text(0).attr('style','font-size:30px');
+								}
+								
+							});
+							var calc = function(){for(var i =0;i<Object.keys(y).length;i++){
+								
+								if($('#book-check-'+i+'').is(':checked')){
+									zz += 1*$( "#input-amount-"+i).spinner("value")*$('#td-tr'+i+'-cart-list-2').text();
+									$('#span-td-0').text(zz).attr('style','font-size:30px');
+									$('#span-td-2').text(zz+$('#span-td-1').text()*1).attr('style','font-size:30px');
+								}
+							}
+							};
+						});
+						$(createDiv({id:'div-btn-wrapper',clazz:''})).appendTo('#div-content');
+						$('#div-btn-wrapper').append(createButton({id:'btn-cart-payment',clazz:'btn',val:'결제하기'}))
+						$('#btn-cart-payment').attr('style','background:#ce6a52;color:#fff; width:250px').click(()=>{
+							
+							shop.mall.sell({
+								view:x.view,
+								context:x.context,
+								val:'02 주문/결제 ',
+								orderPrice:[$('#span-td-0').text(),$('#span-td-1').text(),$('#span-td-2').text()],
+								y:y
+								});
+						})
+						$('#div-btn-wrapper').attr('style','text-align:center; margin:20px');
+						
+						
 					},
 					error:(y,h,m)=>{
 						
 					}
 				});
-				
-				
-				
 
-				
-			
-			
-				
-			/*},
-			error : (x,h,m)=>{
-				alert('검색 실패 x='+x+', h='+h+', m='+m);
-			}
-			});*/
-		$('#div-content').append(createDiv({id:'div-cart-footer',clazz:''}))
-		$(createDiv({id:'div-cart-footer-content',clazz:''})).appendTo('#div-cart-footer')
-		.attr('style','padding-top: 20px');
-		$('#div-cart-footer-content').append(createDiv({
-			id:'div-cart-footer-content-left',
-			clazz:''}))
-		$('#div-cart-footer-content').append(createDiv({
-			id:'div-cart-footer-content-right',
-			clazz:''}))
-			$('#div-cart-footer-content-left').text('부크크의 도서의 경우 주문제작 상품이기 때문에 결제 후 4~5일 정도 배송에 소요됩니다.')
-			$('#div-cart-footer-content-right').append(createButton({
-				id:'btn-select-del',
-				clazz:'btn',val:'선택한 도서 삭제'}))
-		//주문 금액 영역
-		$('#div-content').append(createDiv({id:'div-cart-price',clazz:''}))
-		$('#div-cart-price').append(createTable({id:'table-cart-price',clazz:'table-cart-price'}))
-		$('#table-cart-price').append(createMultiTr({arr:['',''],id:'tr-cart-price'}))
-		$('#tr-cart-price-0').append(createMultiTh({id:'th-price-head',arr:['1','2','3']}))
-		$('#th-price-head-0').text('주문금액');
-		$('#th-price-head-1').text('배송비');
-		$('#th-price-head-2').text('결제 예정금액');
-		$('#tr-cart-price-1').append(createMultiTd({arr:['','',''],id:'td-cart-price'}));
-		$('#td-cart-price-0').append(createSpan({id:'span-td-0',clazz:''})+'원');
-		$('#span-td-0').text('9500').attr('style','font-size:30px');
-		
-		
-		$('#td-cart-price-1').append(createSpan({id:'span-td-1',clazz:''})+'원');
-		$('#span-td-1').attr('style','font-size:30px');
-		$('#td-cart-price-2').append(createSpan({id:'span-td-2',clazz:''})+'원');
-		$('#span-td-2').attr('style','font-size:30px');
-		//배송 checkbox div
-		$('#div-content').append(createDiv({id:'div-delivery-price'}))
-		$('#div-delivery-price').append(createMultiDiv({id:'div-delivery-price',arr:['','']}));
-		$('#div-delivery-price').attr('font-weight','normal');
-		
-		
-		$('#div-delivery-price-0').append(createLabel({
-			fo:'',
-			val:createInput({
-				id:'input-checkbox-0',
-				clazz:'',
-				type:'checkbox'})+'배송료를 선불로 지불합니다. (기본 2,500원)'}));
-		$('#input-checkbox-0').attr('value','2500');
-		$('#input-checkbox-0').attr('checked','checked');
-		$('#div-delivery-price-1').append(createLabel({
-			fo:'',
-			val:createInput({
-				id:'input-checkbox-1',
-				clazz:'',
-				type:'checkbox'})+'제주도 및 도서산간지역(+2,500원)'}));
-		$('#input-checkbox-1').attr('value','2500');
-		$('#span-td-1').text('2500');
-		$('#span-td-2').text(($('#span-td-0').text())*1+($('#span-td-1').text())*1);
-		$('input[id=input-checkbox-0]').click(()=>{
-			if ($('input[id=input-checkbox-0]').is(':checked')) {
-				
-				$('input[id=input-checkbox-1]').removeAttr('disabled');
-				$('#span-td-1').text('2500');
-				$('input[id=input-checkbox-0]').attr('value','2500');
-				$('#span-td-2').text(($('#span-td-0').text())*1+($('#span-td-1').text())*1);
-				
-				
-			} else {
-				
-				$('#span-td-1').text('0');
-				$('input[id=input-checkbox-1]').prop('checked',false);
-				$('input[id=input-checkbox-1]').attr('disabled','disabled');
-				$('#span-td-2').text(($('#span-td-0').text())*1+($('#span-td-1').text())*1);
-			}
-		});
-		$('input[id=input-checkbox-1]').click(()=>{
-			if ($('input[id=input-checkbox-1]').is(':checked')) {
-				
-				$('input[id=input-checkbox-1]').removeAttr('disabled');
-				$('input[id=input-checkbox-1]').attr('value','2500');
-				$('#span-td-1').text(($('input[id=input-checkbox-0]').val())*1+($('input[id=input-checkbox-1]').val())*1);
-				$('#span-td-2').text(($('#span-td-0').text())*1+($('#span-td-1').text())*1);
-				 
-			}else{
-				
-				$('#span-td-1').text(parseInt($('input[id=input-checkbox-0]').val()));
-				$('#span-td-2').text(($('#span-td-0').text())*1+($('#span-td-1').text())*1);
-			}
-		});
-		$(createDiv({id:'div-btn-wrapper',clazz:''})).appendTo('#div-content');
-		$('#div-btn-wrapper').append(createButton({id:'btn-cart-payment',clazz:'btn',val:'결제하기'}))
-		$('#btn-cart-payment').attr('style','background:#ce6a52;color:#fff; width:250px').click(()=>{
-			
-			shop.mall.sell({
-				view:x.view,
-				context:x.context,
-				val:'02 주문/결제 ',
-				orderPrice:[$('#span-td-0').text(),$('#span-td-1').text(),$('#span-td-2').text()],
-				});
-		})
-		$('#div-btn-wrapper').attr('style','text-align:center; margin:20px');
-			
-		
-		
-		
-		
-		//디스에이블 체크박스
-		/*if ($('input[name=sampleCheckbox]').is(":checked")) {
-		    $('input[name=sampleHidden]').val('Y');
-		} else {
-		    $('input[name=sampleHidden]').val('N');
-		}*/
 	});
 	
 	
 },
 sell:x=>{
 	$.getScript(x.view,()=>{
+		
 		$('div[id=div-content]').html(createMultiDiv({
 			id:'div-sell',
 			clazz:'',
@@ -259,6 +299,7 @@ sell:x=>{
 			$('#th-tr0-cart-price-1').text('배송비')
 			$('#th-tr0-cart-price-2').text('결제 예정금액')
 			$('#tr-cart-price-1').append(createMultiTd({id:'td-tr1-cart-price',arr:['','','']}));
+			
 			for(var i =0;i<=2;i++){
 				$('#td-tr1-cart-price-'+i+'').html(createSpan({id:'span-td-'+i+'',clazz:''})+'원');
 				$('#span-td-'+i+'').text(x.orderPrice[i]).attr('style','font-size:30px');
